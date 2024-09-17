0094fec2680a1e80e8f6adf78f669c5ad5133770
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-09-17 16:47:46 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Ex1/src/tp0_ex1/TP0_Ex1.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/**
+ *GAUBIL
+*TDC
+*TP0 exercice 1 
+* 17 septembre 2024 
+*/
+
+
+/**
+ *
+ * @author vtino
+ */
+public class TP0_Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Bonjour");
+       System.out.println("Au-revoir");
+       String prenom;
+       Scanner sc;
+       sc = new Scanner(System.in);
+       System.out.println("Bonjour, quel est votre prenom ?");
+       prenom = sc.nextLine();
+      
+
+    }
+    
+}
+
+
+
+

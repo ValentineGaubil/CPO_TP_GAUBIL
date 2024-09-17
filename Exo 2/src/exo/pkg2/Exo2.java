2c2e1ef17393d04f10e1fa43f7d8e970fb47a1d3
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-09-17 17:09:14 +0200

    Création d'un nouveau projet versionné

---
+++ Exo 2/src/exo/pkg2/Exo2.java
@@ -0,0 +1,37 @@
+/*
+ * GAUBIL
+ * TDC
+ * TP0 exercice 1 
+ * 17 septembre 2024
+ */
+package exo.pkg2;
+
+/**
+ *
+ * @author vtino
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     {
+ //Declaration des variables
+ int nb; // nombre dentiers a additionner
+ int result; // resultat
+ int ind; //indice
+ nb=5;
+ result=0;
+ // Addition des nb premiers entiers
+ ind=1;
+ while (ind <= nb) {
+ result=result+ind;
+ }
+ // Affichage du resultat
+ System.out.println();
+ System.out.println("La somme des "+ nb + "entiers est: "+result);
+ }
+    }
+    
+}

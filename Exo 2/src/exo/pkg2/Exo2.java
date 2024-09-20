ec98fe9fbeb353b1ee0fad65e89fa56800c18be8
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-09-20 12:31:07 +0200

    Création d'un nouveau projet versionné

--- Exo 2/src/exo/pkg2/Exo2.java
+++ Exo 2/src/exo/pkg2/Exo2.java
@@ -6,6 +6,8 @@
  */
 package exo.pkg2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author vtino
@@ -16,22 +18,29 @@ public class Exo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     {
- //Declaration des variables
- int nb; // nombre dentiers a additionner
- int result; // resultat
- int ind; //indice
- nb=5;
- result=0;
- // Addition des nb premiers entiers
- ind=1;
- while (ind <= nb) {
- result=result+ind;
- }
- // Affichage du resultat
- System.out.println();
- System.out.println("La somme des "+ nb + "entiers est: "+result);
- }
+    {
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+        //nb=5;
+        result=0;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+        
+        // Addition des nb premiers entiers
+        ind=1;
+        while (ind <= nb) {
+        result=result+ind;
+        ind = ind + 1;
+        }
+        // Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result);
+    
+    }
     }
     
 }

98972eaa70ceb2a0444bb0e24087da507b0ce6d4
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-09-20 12:56:15 +0200

    Création d'un nouveau projet versionné

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,51 @@
+/*
+/**
+*GAUBIL Valentine
+*TDC
+*TP0 exercice 3 calculator 
+* 20 septembre 2024 
+ */
+package calculator;
+
+/**
+ *
+ * @author vtino
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" Pleaseentertheoperator:");
+        System.out.println("1)add");
+        System.out.println("2)substract");
+        System.out.println("3)multiply");
+        System.out.println("4)divide");
+        System.out.println("5)modulo");
+        
+        int opérator;
+        int operande1;
+        int operande2;
+         System.out.println("veuillez entrer un nombre");
+          System.out.println("veuillez entrer un nombre");
+          
+        
+        
+                
+         
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}

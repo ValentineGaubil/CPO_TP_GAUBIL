1a93f2623b3d5deee3ba5bd4b7d9f52af0c43fe8
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-10-23 11:56:08 +0200

    Création d'un nouveau projet versionné

--- TP1/TP2_biere_GaubilValentine/src/tp2_biere_gaubilvalentine/TP2_biere_GaubilValentine.java
+++ TP1/TP2_biere_GaubilValentine/src/tp2_biere_gaubilvalentine/TP2_biere_GaubilValentine.java
@@ -17,27 +17,39 @@ public class TP2_biere_GaubilValentine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    //bouteillebiere uneBiere = new bouteillebiere(); 
-    //uneBiere.nom = "Cuvée des trolls"; 
-    //uneBiere.degresalcool= 7.0; 
-    //uneBiere.brasserie="debuisson";
-    //uneBiere.ouverte= false;
-    //uneBiere.lireEtiquette(); 
+        //BouteilleBiere uneBiere = new BouteilleBiere() ;
+        //uneBiere.nom = "Cuvee des trolls";
+        //uneBiere.degreAlcool = 7.0 ;
+        //uneBiere.brasserie = "Dubuisson";
+        //uneBiere.ouverte = false;
+        //uneBiere.lireEtiquette();
        
-    
-   //bouteillebiere secondeBiere = new bouteillebiere() ; 
-    // secondeBiere.nom = "leffe"; 
-    //secondeBiere.degresalcool= 6.6; 
-    //secondeBiere.brasserie="abbaye de leffe";
-    //secondeBiere.ouverte= false;
-    //secondeBiere.lireEtiquette();
-    
-    bouteillebiere autreBiere = new  bouteillebiere("Leffe", 6.6 ,"Abbaye de Leffe") ; 
-    autreBiere.lireEtiquette();
-    
-  
-    
+        //BouteilleBiere secondeBiere = new BouteilleBiere() ;
+        //secondeBiere.nom = "Leffe";
+        //secondeBiere.degreAlcool = 6.6 ;
+        //secondeBiere.brasserie = "Abbaye de Leffe";
+        //secondeBiere.ouverte = false;
+        //secondeBiere.lireEtiquette();
+       
+        bouteillebiere uneBiere = new bouteillebiere("Cuvee des trolls",7.0 ,"Dubuisson") ;
+        uneBiere.lireEtiquette();
+        bouteillebiere autreBiere = new bouteillebiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        autreBiere.lireEtiquette();
+        bouteillebiere troisiemeBiere = new bouteillebiere("Heineken" ,5 ,"Gerard Heineken") ;
+        troisiemeBiere.lireEtiquette();
+        bouteillebiere quatriemeBiere = new bouteillebiere("Desperados" ,5.9 ,"Alken-Maes") ;
+        quatriemeBiere.lireEtiquette();
+        bouteillebiere derniereBiere = new bouteillebiere("Corona" ,4.5 ,"Grupo Modelo") ;
+        derniereBiere.lireEtiquette();
+       
+        uneBiere.decapsuler();
+        autreBiere.decapsuler();
+        quatriemeBiere.decapsuler();
+       
+        System.out.println(uneBiere);
+        System.out.println(autreBiere);
+        System.out.println(troisiemeBiere);
+        System.out.println(quatriemeBiere);
+        System.out.println(derniereBiere);
     }
-    
 }
--- TP1/TP2_biere_GaubilValentine/src/tp2_biere_gaubilvalentine/bouteillebiere.java
+++ TP1/TP2_biere_GaubilValentine/src/tp2_biere_gaubilvalentine/bouteillebiere.java
@@ -12,35 +12,40 @@ package tp2_biere_gaubilvalentine;
  * @author vtino
  */
 public class bouteillebiere {
-    String nom;
+    String nom ;
     String brasserie;
-    double degresalcool;
-    boolean ouverte;   
-    
-    
-    public void lireEtiquette() { 
-        System.out.println("Bouteille de " + nom +" (" + degresalcool +" degres) \nBrasserie : " + brasserie ) ; 
-    } 
-    
-    
-   public bouteillebiere(String unNom, double unDegre, String uneBrasserie) {
-    nom = unNom; 
-    degresalcool = unDegre; 
-    brasserie = uneBrasserie; 
-    ouverte = false; 
-}
-
- 
-
-    
-} {
-    nom = unNom; 
-    degreAlcool = unDegre; 
-    brasserie = uneBrasserie; 
-    ouverte = false; 
-}
-
- 
-
-    
+    double degreAlcool;
+    boolean ouverte ;
+   
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) ");
+        System.out.println("Brasserie : " + brasserie ) ;
+    }
+   
+    public bouteillebiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+   
+    public void decapsuler() {
+        if (ouverte == false) {
+            ouverte = true;
+        }
+        else if (ouverte == true) {
+            System.out.println("erreur : biere deja ouverte");
+            ouverte = false;
+        }
+        System.out.println(ouverte);
+    }
+   
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres) Ouverte ? ";
+        if (ouverte == true ) chaine_a_retourner += "oui" ;
+        else chaine_a_retourner += "non" ;
+        return chaine_a_retourner;
+    }
 }

6528641a975c99a4d3b985fb7e9a7beee3e71ac1
valentine Gaubil <valentine.gaubil@epfedu.fr>
2024-11-06 10:21:20 +0100

    Création d'un nouveau projet versionné

---
+++ TP1/ TP2_manip_Gaubilvalentine/src/tp2_manip_gaubilvalentine/Moussaka.java
@@ -0,0 +1,22 @@
+package tp2_manip_gaubilvalentine;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vtino
+ */
+public class Moussaka {
+     
+    int nbCalories;
+   
+    // Constructeur pour initialiser nbCalories
+    public Moussaka(int calories) {
+        this.nbCalories = calories;
+    }
+
+    
+}
